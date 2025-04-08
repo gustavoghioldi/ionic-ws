ec7ec2ba51586b8f685a917ffabbfe90599ccb47
gustavoghioldi <gustavoghioldi@gmail.com>
2025-04-08 20:55:50 -0300

    featch

--- ionic-ws/src/views/HomePage.vue
+++ ionic-ws/src/views/HomePage.vue
@@ -26,6 +26,15 @@ const clickMeHandler = () => {
   console.log('Button clicked!');
 };
 
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
 
 </script>
 
